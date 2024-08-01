cf2546caead99ca9602273f42af667ca1af45eef
Tyler Byrd <tyler.byrd@cvent.com>
2024-08-01 16:08:09 -0400

    Started the project

---
+++ src/main/java/org/cventbootcamp/Application.java
@@ -0,0 +1,19 @@
+package org.cventbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import org.cventbootcamp.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "org.cventbootcamp.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
